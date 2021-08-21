6485d26482d2d8501c0ca2991854585ebe6884b9
Tomasz <40279346+rastuszek@users.noreply.github.com>
2021-08-21 14:46:00 +0200

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -106,7 +106,7 @@ const Login = (props) => {
                 Zaloguj
               </Button>
               <p>{en.FORM_FIELDS.LOGIN_ALT_TEXT}</p>
-              <Link to="/rejestracja">Zarejestrujd się</Link>
+              <Link to="/rejestracja">Zarejestruj się</Link>
             </Grid>
           </Grid>
         </form>
